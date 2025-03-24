5238d6846f9c0dde5676ca2546f75065fe6104a8
stvbex <12851562+stvbex@users.noreply.github.com>
2025-03-24 10:34:23 +0200

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  blue: 10,
+  red: 10,
+  green: 10
+}
+
+console.log(color);
+
+export default color;
